01a1ae96ac234558cc48826d5f75f8d10174612e
Muhammad Musa <muhammad.musa@auxcube.com>
2022-07-08 15:40:55 +0500

    Add wave animation

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const cursor = document.querySelector("#cursor");
 
-document.addEventListener("mousemove", (e) => {
-  cursor.classList.remove("hidden");
-  cursor.setAttribute(
-    "style",
-    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
-  );
-});
+// setTimeout(() => {
+//   document.addEventListener("mousemove", (e) => {
+//     if (window.innerWidth < 768) return;
+
+//     cursor.src = "./imgs/pointing-hand-tilted.png";
+
+//     cursor.setAttribute(
+//       "style",
+//       `top: ${e.pageY}px; left: ${e.pageX}px; animation: none`
+//     );
+//   });
+// }, 3000);
