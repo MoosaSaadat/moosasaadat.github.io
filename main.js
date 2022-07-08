b053d2f6509dea73d0f94ebfd3f98a474789fd3e
Muhammad Musa <muhammad.musa@auxcube.com>
2022-07-08 23:24:12 +0500

    Position translation on cursor

--- main.js
+++ main.js
@@ -1,67 +1,76 @@
-// const cursor = document.querySelector("#cursor");
-
-// setTimeout(() => {
-//   document.addEventListener("mousemove", (e) => {
-//     if (window.innerWidth < 768) return;
-
-//     cursor.src = "./imgs/pointing-hand-tilted.png";
-
-//     cursor.setAttribute(
-//       "style",
-//       `top: ${e.pageY}px; left: ${e.pageX}px; animation: none`
-//     );
-//   });
-// }, 3000);
 const translation = document.querySelector("#translation");
+
+let translationPos;
 
 const showTranslation = (text, position) => {
   translation.innerHTML = text;
   translation.classList.remove("hidden");
-  translation.classList.add("bottom");
+  translationPos = position;
+
+  if (translationPos === "top") {
+    translation.classList.remove("bottom");
+    translation.classList.add("top");
+  } else if (translationPos === "bottom") {
+    translation.classList.remove("top");
+    translation.classList.add("bottom");
+  }
 };
 const hideTranslation = () => {
   translation.classList.add("hidden");
 };
+const changeTranslationPos = (x, y) => {
+  let newPos = "";
+  if (translationPos === "top")
+    newPos = `left: ${x}px; bottom: calc(100vh - ${y}px + 50px)`;
+  else if (translationPos === "bottom")
+    newPos = `left: ${x}px; top: calc(${y}px + 50px)`;
 
-const title = document.querySelector("#title");
-const subtitle = document.querySelector("#subtitle");
+  translation.setAttribute("style", newPos);
+};
+
+const fullname = document.querySelector("#name");
+const designation = document.querySelector("#designation");
 
 let titleInterval;
 let subtitleInterval;
 
-title.addEventListener("mouseenter", (e) => {
+fullname.addEventListener("mouseenter", (e) => {
   let counter = 0;
   titleInterval = setInterval(() => {
     const newText = TITLES[counter].text;
     counter = (counter + 1) % TITLES.length;
-    showTranslation(newText);
+    showTranslation(newText, "top");
   }, 500);
+  changeTranslationPos(e.pageX, e.pageY);
 });
-title.addEventListener("mouseleave", (e) => {
+fullname.addEventListener("mouseleave", (e) => {
   clearInterval(titleInterval);
   hideTranslation();
 });
 
-subtitle.addEventListener("mouseenter", (e) => {
+designation.addEventListener("mouseenter", (e) => {
   let counter = 0;
   subtitleInterval = setInterval(() => {
     const newText = SUBTITLES[counter].text;
     counter = (counter + 1) % SUBTITLES.length;
-    showTranslation(newText);
+    showTranslation(newText, "bottom");
   }, 500);
+  changeTranslationPos(e.pageX, e.pageY);
 });
-subtitle.addEventListener("mouseleave", (e) => {
+designation.addEventListener("mouseleave", (e) => {
   clearInterval(subtitleInterval);
   hideTranslation();
 });
 
+const cursor = document.querySelector("#cursor");
 document.addEventListener("mousemove", (e) => {
   if (window.innerWidth < 768) return;
 
   cursor.src = "./imgs/pointing-hand-tilted.png";
-
   cursor.setAttribute(
     "style",
     `top: ${e.pageY}px; left: ${e.pageX}px; animation: none`
   );
+
+  changeTranslationPos(e.pageX, e.pageY);
 });
