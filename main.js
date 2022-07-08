7c0841afc4adad0edf704f6e0932e78e5a60e971
Muhammad Musa <muhammad.musa@auxcube.com>
2022-07-08 21:40:47 +0500

    Add translation box

--- main.js
+++ main.js
@@ -12,189 +12,56 @@
 //     );
 //   });
 // }, 3000);
+const translation = document.querySelector("#translation");
 
-const firstName = document.querySelector("#first-name");
-const lastName = document.querySelector("#last-name");
-const title = document.querySelector("#subtitle");
+const showTranslation = (text, position) => {
+  translation.innerHTML = text;
+  translation.classList.remove("hidden");
+  translation.classList.add("bottom");
+};
+const hideTranslation = () => {
+  translation.classList.add("hidden");
+};
 
-let firstNameIdx = 0;
-let lastNameIdx = 0;
-let titleIdx = 0;
+const title = document.querySelector("#title");
+const subtitle = document.querySelector("#subtitle");
 
-const FIRST_NAMES = [
-  {
-    language: "English",
-    symbol: "en",
-    text: "Muhammad",
-  },
-  {
-    language: "Urdu",
-    symbol: "ur",
-    text: "محمد",
-  },
-  {
-    language: "Dutch",
-    symbol: "nl",
-    text: "Mohammed",
-  },
-  {
-    language: "French",
-    symbol: "fr",
-    text: "Mahomet",
-  },
-  {
-    language: "German",
-    symbol: "de",
-    text: "Mohammed",
-  },
-  {
-    language: "Italian",
-    symbol: "it",
-    text: "Maometto",
-  },
-  {
-    language: "Japanese",
-    symbol: "ja",
-    text: "ムハンマド",
-  },
-  {
-    language: "Russian",
-    symbol: "ru",
-    text: "Мухаммед",
-  },
-  {
-    language: "Spanish",
-    symbol: "es",
-    text: "Mahoma",
-  },
-];
-const LAST_NAMES = [
-  {
-    language: "English",
-    symbol: "en",
-    text: "Musa",
-  },
-  {
-    language: "Urdu",
-    symbol: "ur",
-    text: "موسیٰ",
-  },
-  {
-    language: "Dutch",
-    symbol: "nl",
-    text: "Musa",
-  },
-  {
-    language: "French",
-    symbol: "fr",
-    text: "Moussa",
-  },
-  {
-    language: "German",
-    symbol: "de",
-    text: "Musa",
-  },
-  {
-    language: "Italian",
-    symbol: "it",
-    text: "Musa",
-  },
-  {
-    language: "Japanese",
-    symbol: "ja",
-    text: "ムーサ",
-  },
-  {
-    language: "Russian",
-    symbol: "ru",
-    text: "Муса",
-  },
-  {
-    language: "Spanish",
-    symbol: "es",
-    text: "musa",
-  },
-];
-const TITLES = [
-  {
-    language: "English",
-    symbol: "en",
-    text: "Software Engineer",
-  },
-  {
-    language: "Urdu",
-    symbol: "ur",
-    text: "سافٹ ویئر انجینئر",
-  },
-  {
-    language: "Dutch",
-    symbol: "nl",
-    text: "Software ontwikkelaar",
-  },
-  {
-    language: "French",
-    symbol: "fr",
-    text: "Ingénieur logiciel",
-  },
-  {
-    language: "German",
-    symbol: "de",
-    text: "Softwareentwickler",
-  },
-  {
-    language: "Italian",
-    symbol: "it",
-    text: "Ingegnere del software",
-  },
-  {
-    language: "Japanese",
-    symbol: "ja",
-    text: "ソフトウェアエンジニア",
-  },
-  {
-    language: "Russian",
-    symbol: "ru",
-    text: "Программист",
-  },
-  {
-    language: "Spanish",
-    symbol: "es",
-    text: "Ingeniero de software",
-  },
-];
+let titleInterval;
+let subtitleInterval;
 
-const firstNameInterval = setInterval(() => {
-  const translation = FIRST_NAMES[firstNameIdx];
-  if (translation.symbol === "ur") {
-    firstName.setAttribute(
-      "style",
-      "font-family: 'IBM Plex Sans Arabic', sans-serif;"
-    );
-  }
-  firstName.innerHTML = translation.text;
-  firstNameIdx = (firstNameIdx + 1) % FIRST_NAMES.length;
-}, 500);
+title.addEventListener("mouseenter", (e) => {
+  let counter = 0;
+  titleInterval = setInterval(() => {
+    const newText = TITLES[counter].text;
+    counter = (counter + 1) % TITLES.length;
+    showTranslation(newText);
+  }, 500);
+});
+title.addEventListener("mouseleave", (e) => {
+  clearInterval(titleInterval);
+  hideTranslation();
+});
 
-setInterval(() => {
-  const translation = LAST_NAMES[lastNameIdx];
-  if (translation.symbol === "ur") {
-    lastName.setAttribute(
-      "style",
-      "font-family: 'IBM Plex Sans Arabic', sans-serif;"
-    );
-  }
-  lastName.innerHTML = translation.text;
-  lastNameIdx = (lastNameIdx + 1) % LAST_NAMES.length;
-}, 500);
+subtitle.addEventListener("mouseenter", (e) => {
+  let counter = 0;
+  subtitleInterval = setInterval(() => {
+    const newText = SUBTITLES[counter].text;
+    counter = (counter + 1) % SUBTITLES.length;
+    showTranslation(newText);
+  }, 500);
+});
+subtitle.addEventListener("mouseleave", (e) => {
+  clearInterval(subtitleInterval);
+  hideTranslation();
+});
 
-setInterval(() => {
-  const translation = TITLES[titleIdx];
-  if (translation.symbol === "ur") {
-    title.setAttribute(
-      "style",
-      "font-family: 'IBM Plex Sans Arabic', sans-serif;"
-    );
-  }
-  title.innerHTML = translation.text;
-  titleIdx = (titleIdx + 1) % TITLES.length;
-}, 500);
+document.addEventListener("mousemove", (e) => {
+  if (window.innerWidth < 768) return;
+
+  cursor.src = "./imgs/pointing-hand-tilted.png";
+
+  cursor.setAttribute(
+    "style",
+    `top: ${e.pageY}px; left: ${e.pageX}px; animation: none`
+  );
+});
---
+++ translations.js
@@ -0,0 +1,84 @@
+const TITLES = [
+  {
+    language: "English",
+    symbol: "en",
+    text: "Muhammad Musa",
+  },
+  {
+    language: "Urdu",
+    symbol: "ur",
+    text: "محمد موسیٰ",
+  },
+  {
+    language: "Dutch",
+    symbol: "nl",
+    text: "Mohammed Musa",
+  },
+  {
+    language: "French",
+    symbol: "fr",
+    text: "Muhammad Moussa",
+  },
+  {
+    language: "Japanese",
+    symbol: "ja",
+    text: "ムハンマドムサ",
+  },
+  {
+    language: "Russian",
+    symbol: "ru",
+    text: "Мухаммад Муса",
+  },
+  {
+    language: "Spanish",
+    symbol: "es",
+    text: "muhammad musa",
+  },
+];
+const SUBTITLES = [
+  {
+    language: "English",
+    symbol: "en",
+    text: "Software Engineer",
+  },
+  {
+    language: "Urdu",
+    symbol: "ur",
+    text: "سافٹ ویئر انجینئر",
+  },
+  {
+    language: "Dutch",
+    symbol: "nl",
+    text: "Software ontwikkelaar",
+  },
+  {
+    language: "French",
+    symbol: "fr",
+    text: "Ingénieur logiciel",
+  },
+  {
+    language: "German",
+    symbol: "de",
+    text: "Softwareentwickler",
+  },
+  {
+    language: "Italian",
+    symbol: "it",
+    text: "Ingegnere del software",
+  },
+  {
+    language: "Japanese",
+    symbol: "ja",
+    text: "ソフトウェアエンジニア",
+  },
+  {
+    language: "Russian",
+    symbol: "ru",
+    text: "Программист",
+  },
+  {
+    language: "Spanish",
+    symbol: "es",
+    text: "Ingeniero de software",
+  },
+];
