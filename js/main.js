a3e328d8b1edd1e63a52df6ae02a2c4bdd6d42e1
Muhammad Musa <muhammad.musa@auxcube.com>
2022-07-14 18:53:30 +0500

    Remove horizontal cursor overflow

--- js/main.js
+++ js/main.js
@@ -66,11 +66,8 @@ const cursor = document.querySelector("#cursor");
 document.addEventListener("mousemove", (e) => {
   if (window.innerWidth < 768) return;
 
-  // cursor.src = "./imgs/pointing-hand-tilted.png";
   cursor.setAttribute(
     "style",
     `top: ${e.pageY + 2}px; left: ${e.pageX + 2}px; animation: none`
   );
-
-  // changeTranslationPos(e.pageX, e.pageY);
 });
